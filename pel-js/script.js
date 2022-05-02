e2c4453f0ea1bf6a9b6580a5a996a54e72ff1bd6
Marcos Pelaez <pelaez.marcos@gmail.com>
2022-05-02 17:52:49 -0300

    Site completo
    Site totalmente completo. desenvolvido.

---
+++ pel-js/script.js
@@ -0,0 +1,34 @@
+
+//Functions for chenge socials buttons on hover
+
+//Linkedin
+function chengeLinkedinOver() {
+    document.getElementById("linkedin").src="pel-img/linkedin-bt-hover.png";
+}
+function backNormalLinkedin() {
+    document.getElementById("linkedin").src="pel-img/linkedin-bt.png";
+}
+
+//Facebook
+function chengeFacebookOver() {
+    document.getElementById("facebook").src="pel-img/facebook-bt-hover.png";
+}
+function backNormalfacebook() {
+    document.getElementById("facebook").src="pel-img/facebook-bt.png";
+}
+
+//Linkedin-footer
+function chengeLinkedinOverFooter() {
+    document.getElementById("linkedin-footer").src="pel-img/linkedin-bt-hover.png";
+}
+function backNormalLinkedinFooter() {
+    document.getElementById("linkedin-footer").src="pel-img/linkedin-bt.png";
+}
+
+//Facebook-footer
+function chengeFacebookOverFooter() {
+    document.getElementById("facebook-footer").src="pel-img/facebook-bt-hover.png";
+}
+function backNormalfacebookFooter() {
+    document.getElementById("facebook-footer").src="pel-img/facebook-bt.png";
+}
